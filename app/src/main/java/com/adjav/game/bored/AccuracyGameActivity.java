cb103b07f69d38f1a0d06903459913d70cc035de
Carlson <carlsontabije@gmail.com>
2020-01-23 15:03:01 +0800

    Accuracy Game Update

--- app/src/main/java/com/adjav/game/bored/AccuracyGameActivity.java
+++ app/src/main/java/com/adjav/game/bored/AccuracyGameActivity.java
@@ -192,6 +192,23 @@ public class AccuracyGameActivity extends AppCompatActivity {
                 sound.playHitSound();
             }
         });
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
     }
 
     private void theGameActions(){
