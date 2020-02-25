1492ec7036541152d4271b9897987005d5fb3a36
Carina Tabije <carlsontabije>
2020-02-26 03:05:52 +0800

    accuracy game multiplayer added

---
+++ app/src/main/java/com/adjav/game/bored/AccuracyRoom.java
@@ -0,0 +1,122 @@
+package com.adjav.game.bored;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccuracyRoom extends AppCompatActivity {
+
+    ListView listView;
+    Button button;
+
+    List<String> roomsList;
+
+    String playerName = "";
+    String roomName = "";
+
+    FirebaseDatabase db;
+    DatabaseReference roomRef;
+    DatabaseReference roomsRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accuracy_room);
+
+        db = FirebaseDatabase.getInstance();
+
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        playerName = preferences.getString("playerName", "");
+        roomName = playerName;
+
+        listView = findViewById(R.id.ListView);
+        button = findViewById(R.id.button);
+
+        roomsList = new ArrayList<>();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("CREATING ROOM");
+                button.setEnabled(false);
+                roomName = playerName;
+                roomRef = db.getReference("rooms/" + roomName + "/player 1");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                roomName = roomsList.get(position);
+                roomRef = db.getReference("rooms/" + roomName + "/player 1");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+            }
+        });
+
+        addRoomsEventListener();
+    }
+
+    private void addRoomEventListener(){
+        roomRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                button.setText("CREATING ROOOM");
+                button.setEnabled(true);
+                Intent intent = new Intent(getApplicationContext(), AccuracyMultiplayer.class);
+                intent.putExtra("roomName", roomName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                button.setText("CREATE ROOM");
+                button.setEnabled(true);
+                Toast.makeText(AccuracyRoom.this, "ERROR!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void addRoomsEventListener(){
+        roomsRef = db.getReference("rooms");
+        roomsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                roomsList.clear();
+                Iterable<DataSnapshot> rooms = dataSnapshot.getChildren();
+                for(DataSnapshot snapshot : rooms){
+                    roomsList.add(snapshot.getKey());
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(AccuracyRoom.this,
+                            android.R.layout.simple_list_item_1, roomsList);
+                    listView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
